f48ec03f9f1824fbfcb2ad0e34010d5d4548bbfa
tunar-12 <Tunar@MSI>
2024-01-03 00:53:21 -0800

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
   title = 'Dating App';
-  users: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-
-    // get returns observable and if we need to see what is in observable we need subscribe()
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request completed successfully!')
-    })
+    this.setCurrentUser();
   }
 
+  
+  setCurrentUser()
+  {
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user: User = JSON.parse(userString);  
+    this.accountService.setCurrentUser(user);
+    console.log("sdaadsda"+ userString);
+  }
   
 }
---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  users:any;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  registerToggle()
+  {
+    this.registerMode = !this.registerMode;
+  }
+
+  getUsers()
+  {
+    // get returns observable and if we need to see what is in observable we need subscribe()
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request completed successfully!')
+    })
+  }
+
+  cancelRegisterMode(event: boolean){
+    this.registerMode = event;
+  }
+}
