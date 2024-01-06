ac61bfc2acffe9549b3a5bc14c26cc6fb0272892
tunar-12 <Tunar@MSI>
2024-01-06 12:05:37 -0800

    End of section 8

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,5 +27,24 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+
+// with help of these whenever we drop our database we will get back our seeding datas
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch(Exception ex)
+{
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "An error occured during migration!");
+}
+
+// seeding data ends
 
 app.Run();
